0ddeca8cfc78a37fb436326635f00b3280168e8f
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-05-15 12:46:48 +0200

    speed adjusted when typing two moving direction at the same time

--- src_bonus/doors_bonus.c
+++ src_bonus/doors_bonus.c
@@ -6,7 +6,7 @@
 /*   By: blarger <blarger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:48:23 by blarger           #+#    #+#             */
-/*   Updated: 2024/05/15 09:34:43 by blarger          ###   ########.fr       */
+/*   Updated: 2024/05/15 12:38:26 by blarger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ void	open_door(t_data *data)
 	if (data->player_can_open_door == true
 		&& data->map[(int)y][(int)x] == DOOR_CLOSED)
 	{
-		printf("open door data->map[%f][%f] = %c\n", y, x, data->map[(int)y][(int)x]);
 		data->map[(int)data->y_door_op][(int)data->x_door_op] = DOOR_OPEN;
 		data->player_can_open_door = false;
 		data->door_is_open = true;
@@ -48,8 +47,6 @@ void	open_door(t_data *data)
 			paint_minimap(data);
 		}
 	}
-	else
-		printf("Failed to locate door\n");//to delete, testing purpose
 }
 
 void	check_player_can_open_door(t_data *data, t_pfv pfv)//can delete
@@ -60,7 +57,6 @@ void	check_player_can_open_door(t_data *data, t_pfv pfv)//can delete
 		data->player_can_open_door = true;
 		data->x_door_op = pfv.obs_x;
 		data->y_door_op = pfv.obs_y;
-		//printf("can open door\n");
 	}
 	else if (pfv.i == WIDTH / 2)
 	{
